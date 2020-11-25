d1ab9c69b4f854a67589c25c6a7efcfeb8dae033
hermioneee2 <hermioneee2@gmail.com>
2020-11-25 14:22:29 +0900

    20201125 class

---
+++ JustSketchIt-2XX-Archive/JSI202_2020__1125_1416_Hello/Scripts/JSI/JSIApp.cs
@@ -0,0 +1,24 @@
+﻿using X;
+using UnityEngine;
+
+namespace JSI {
+    public class JSIApp : XApp {
+        public override XLogMgr getLogMgr() {
+            throw new System.NotImplementedException();
+        }
+
+        public override XScenarioMgr getScenarioMgr() {
+            throw new System.NotImplementedException();
+        }
+
+        //called once
+        private void Start() {
+            Debug.Log("Hello, World!");
+        }
+
+        //called every time
+        private void Update() {
+
+        }
+    }
+}
